eb14d2c4782d31507fa61de31f8d7b02d43c406c
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2024-09-27 15:43:45 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,14 +1,21 @@
 import sys
 
-#아스키 코드로 a ~ z를 키로 등록
-alp_dict = {chr(97 + i): i for i in range(27)}
+# 아스키 코드로 a ~ z를 키로 등록
+alp_dict = {chr(97 + i): i + 1 for i in range(26)}  # a=1, b=2, ..., z=26
 total = 0
 
+# 상수 정의
+r = 31
+M = 1234567891
+
+# 입력 받기
 N = int(sys.stdin.readline().strip())
 input_data = list(sys.stdin.readline().strip())
-    
-for alp in range(0,len(input_data)):
-    count = ((alp_dict[input_data[alp]]+ 1) * (31**(alp))) 
-    total += count
-    
+
+# 해시 값 계산
+for i in range(N):
+    # 각 알파벳의 값에 31의 i승을 곱하고 M으로 나눠서 나머지를 구함
+    total += (alp_dict[input_data[i]] * (r ** i)) % M
+    total %= M  # 계속해서 M으로 나누어 나머지를 유지
+
 print(total)
