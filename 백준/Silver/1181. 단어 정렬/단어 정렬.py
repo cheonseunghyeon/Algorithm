8659b58f7c810c605e2a0bd6ee81588cf3a050d8
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2024-10-04 14:59:28 +0900

    [Silver V] Title: 단어 정렬, Time: 160 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,16 +3,24 @@ import sys
 def read():
     return sys.stdin.readline().strip()
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if len(x) < len(pivot) or (len(x) == len(pivot) and x < pivot)]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if len(x) > len(pivot) or (len(x) == len(pivot) and x > pivot)]
+    
+    return quick_sort(left) + middle + quick_sort(right)
+
 N = int(read())
 Input = [read() for _ in range(N)]
+
+# 중복 제거
 Input = list(set(Input))
 
-Input.sort()  
-for i in range(1, len(Input)):
-    for j in range(i, 0, -1):
-        if len(Input[j]) < len(Input[j-1]):
-            Input[j], Input[j-1] = Input[j-1], Input[j]
+sorted_input = quick_sort(Input)
 
-
-for item in Input:
+for item in sorted_input:
     print(item)
